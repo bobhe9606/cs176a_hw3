cfa01ac1e00c4b7ac9a6094eec57d6c8bed6b7ba
bobhe9606 <roberthe9606@gmail.com>
2022-05-01 16:08:45 -0700

    first commit

---
+++ PingClient.py
@@ -0,0 +1,24 @@
+import random
+from socket import *
+
+serverName = sys.argv[1]
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+connected = True
+print('from', serverName, ':', 'icmp_seq=', seq_num, 'time=', time, 'ms')
+while connected:
+    if :
+        print('---', serverName, 'ping statistics ---')
+        print(num_packets, 'packets transmitted, ', pack_received, 'packets received', pack_loss, '% packet loss')
+        print('round-trip min/avg/max/stddev =', min, '/', 'avg', avg, 'max', max, 'ms')
+        connected = False
+        clientSocket.close()
+    else:
+
+        connected = False
+        clientSocket.close()
+
+
+
+
+
