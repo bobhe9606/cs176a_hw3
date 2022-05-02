3467113326c2a85decb413465e1b77edbb2fbb0b
Nick Johnson <njohnson@ucsb.edu>
2022-05-02 07:59:44 -0700

    first attempt at PingClient

--- PingClient.py
+++ PingClient.py
@@ -1,22 +1,48 @@
+from audioop import avg
+from datetime import datetime
 import random
 from socket import *
+from struct import pack
+import sys
+import time
+from datetime import datetime
 
 serverName = sys.argv[1]
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-connected = True
-print('from', serverName, ':', 'icmp_seq=', seq_num, 'time=', time, 'ms')
-while connected:
-    if :
-        print('---', serverName, 'ping statistics ---')
-        print(num_packets, 'packets transmitted, ', pack_received, 'packets received', pack_loss, '% packet loss')
-        print('round-trip min/avg/max/stddev =', min, '/', 'avg', avg, 'max', max, 'ms')
-        connected = False
-        clientSocket.close()
-    else:
+clientSocket.settimeout(1)
+icmp_seq = 0
+rttList = []
+while (icmp_seq < 11):
+    timeStamp = time.time()
+    # timeStamp = int(datetime.now())
 
-        connected = False
-        clientSocket.close()
+    message = "PING {} {}".format(icmp_seq, timeStamp)
+    clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+    try:
+        serverMessage, serverAddress = clientSocket.recvfrom(2048)
+        rtt = time.time() * 1000 - timeStamp * 1000
+        rttList.append(rtt)
+        # rtt = int(datetime.now() * 1000) - timeStamp * 1000
+        print("PING received from {}: seq#={} time={} ms".format(
+        serverName, icmp_seq, round(rtt, 3)))
+    except timeout:
+        print("Request timeout for seq#={}".format(icmp_seq))
+
+
+    icmp_seq += 1
+
+clientSocket.close()
+#https://www.geeksforgeeks.org/find-average-list-python/
+print("-- ping statistics --")
+packetsRcvd = len(rttList)
+packetsPercentLost = (10 - packetsRcvd)  * 10
+minRtt = min(rttList)
+maxRtt = max(rttList)
+avgRtt = sum(rttList) / packetsRcvd
+print("10 packets transmitted, {} received, {}% packet loss rtt min/avg/max = {} {} {} ms".format(packetsRcvd, packetsPercentLost, round(minRtt, 3), round(avgRtt, 3), round(maxRtt, 3)))
+
 
 
 
---
+++ timeTest.py
@@ -0,0 +1,7 @@
+import time
+
+
+t1 = time.time() * 1000
+print(t1)
+t2 = time.time() * 1000
+print(t2)
