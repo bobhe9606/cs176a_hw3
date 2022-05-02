344279ef8b7a1d77303fdf858e6356ce9f916c5d
Nick Johnson <njohnson@ucsb.edu>
2022-05-02 11:50:52 -0700

    PingClient

--- PingClient.py
+++ PingClient.py
@@ -1,21 +1,16 @@
-from audioop import avg
-from datetime import datetime
-import random
 from socket import *
-from struct import pack
 import sys
 import time
-from datetime import datetime
+
 
 serverName = sys.argv[1]
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.settimeout(1)
-icmp_seq = 0
+clientSocket.settimeout(1) #wait at most 1 second before timeout
+icmp_seq = 1
 rttList = []
 while (icmp_seq < 11):
     timeStamp = time.time()
-    # timeStamp = int(datetime.now())
 
     message = "PING {} {}".format(icmp_seq, timeStamp)
     clientSocket.sendto(message.encode(), (serverName, serverPort))
@@ -24,24 +19,29 @@ while (icmp_seq < 11):
         serverMessage, serverAddress = clientSocket.recvfrom(2048)
         rtt = time.time() * 1000 - timeStamp * 1000
         rttList.append(rtt)
-        # rtt = int(datetime.now() * 1000) - timeStamp * 1000
         print("PING received from {}: seq#={} time={} ms".format(
         serverName, icmp_seq, round(rtt, 3)))
+        time.sleep(1)  # 1 second delay between each ping as per instructions
     except timeout:
+        #in case of timeout, then there is already a 1 second delay before sending next ping
         print("Request timeout for seq#={}".format(icmp_seq))
-
 
     icmp_seq += 1
 
 clientSocket.close()
-#https://www.geeksforgeeks.org/find-average-list-python/
-print("-- ping statistics --")
+
+
+#Reference: https://www.geeksforgeeks.org/find-average-list-python/
+print("--- ping statistics ---")
 packetsRcvd = len(rttList)
 packetsPercentLost = (10 - packetsRcvd)  * 10
-minRtt = min(rttList)
-maxRtt = max(rttList)
-avgRtt = sum(rttList) / packetsRcvd
-print("10 packets transmitted, {} received, {}% packet loss rtt min/avg/max = {} {} {} ms".format(packetsRcvd, packetsPercentLost, round(minRtt, 3), round(avgRtt, 3), round(maxRtt, 3)))
+if packetsRcvd > 0:
+    minRtt = min(rttList)
+    maxRtt = max(rttList)
+    avgRtt = sum(rttList) / packetsRcvd
+    print("10 packets transmitted, {} received, {}% packet loss rtt min/avg/max = {} {} {} ms".format(packetsRcvd, packetsPercentLost, round(minRtt, 3), round(avgRtt, 3), round(maxRtt, 3)))
+else:
+    print("10 packets transmitted, 0 received, 100% packet loss"); 
 
 
 
